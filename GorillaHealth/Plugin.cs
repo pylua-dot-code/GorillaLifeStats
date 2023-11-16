a0587dda4f8dda71506734b9e349d3d7183b6b27
ConnorBieszk <147281703+pylua-dot-code@users.noreply.github.com>
2023-11-15 18:02:21 -0600

    Added BaseLifeStats Function

--- GorillaHealth/Plugin.cs
+++ GorillaHealth/Plugin.cs
@@ -1,6 +1,7 @@
 ﻿using BepInEx;
 using System;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 using Utilla;
 
 namespace GorillaLifeStats
@@ -9,46 +10,81 @@ namespace GorillaLifeStats
     [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
     public class Plugin : BaseUnityPlugin
     {
+        public int BaseHealth;
         public int Health;
+        public int BaseStamina;
         public int Stamina;
         bool inRoom;
 
-        public void DamageManager(int AddRemoveSet,  int Value, bool GetHealth)
+        public void DamageManager(int Function,  int Value, bool GetHealth)
         {
-            if (AddRemoveSet == 0)
+            if (Function == 0)
             {
                 Health += Value;
             }
-            if (AddRemoveSet == 1)
+            if (Function == 1)
             {
                 Health -= Value;
             }
-            if (AddRemoveSet == 2)
+            if (Function == 2)
             {
                 Health = Value;
+            }
+            if (Function == 3)
+            {
+                Health = Value;
+                BaseHealth = Health;
             }
             if (GetHealth)
             {
                 Logger.LogInfo("GorillaLifeStats Health is " + Health);
             }
         }
-        public void StaminaManager(int AddRemoveSet, int Value, bool GetStamina)
+        public void StaminaManager(int Function, int Value, bool GetStamina)
         {
-            if (AddRemoveSet == 0)
+            if (Function == 0)
             {
                 Stamina += Value;
             }
-            if (AddRemoveSet == 1)
+            if (Function == 1)
             {
                 Stamina -= Value;
             }
-            if (AddRemoveSet == 2)
+            if (Function == 2)
             {
                 Stamina = Value;
+            }
+            if (Function == 3)
+            {
+                Stamina = Value;
+                BaseStamina = Stamina;
             }
             if (GetStamina)
             {
                 Logger.LogInfo("GorrilaLifeStats Stamina is " + Stamina);
+            }
+        }
+        void Update()
+        {
+            if (Health > BaseHealth)
+            {
+                Health = BaseHealth;
+                Logger.LogWarning("Health was larger than BaseHealth. Setting to BaseHealth");
+            }
+            if (Health < 0)
+            {
+                Health = 0;
+                Logger.LogWarning("Health was smaller than 0, Setting Health to 0");
+            }
+            if (Stamina > BaseStamina)
+            {
+                Stamina = BaseStamina;
+                Logger.LogWarning("Stamina was larger than BaseStamina. Setting to BaseStamina");
+            }
+            if (Health < 0)
+            {
+                Health = 0;
+                Logger.LogWarning("Stamina was smaller than 0, Setting Stamina to 0");
             }
         }
     }
