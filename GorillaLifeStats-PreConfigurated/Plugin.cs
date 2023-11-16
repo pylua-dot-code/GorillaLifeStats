50fc5b39903c2271131f7ed705599c48efb0a74e
ConnorBieszk <147281703+pylua-dot-code@users.noreply.github.com>
2023-11-15 18:21:01 -0600

    Changed Folder Names
    Changed folder name
      - New Folder made (will be for preconfiged version)

---
+++ GorillaLifeStats-PreConfigurated/Plugin.cs
@@ -0,0 +1,91 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using Utilla;
+
+namespace GorillaLifeStats
+{
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.6.10")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public int BaseHealth;
+        public int Health;
+        public int BaseStamina;
+        public int Stamina;
+        bool inRoom;
+
+        public void DamageManager(int Function,  int Value, bool GetHealth)
+        {
+            if (Function == 0)
+            {
+                Health += Value;
+            }
+            if (Function == 1)
+            {
+                Health -= Value;
+            }
+            if (Function == 2)
+            {
+                Health = Value;
+            }
+            if (Function == 3)
+            {
+                Health = Value;
+                BaseHealth = Health;
+            }
+            if (GetHealth)
+            {
+                Logger.LogInfo("GorillaLifeStats Health is " + Health);
+            }
+        }
+        public void StaminaManager(int Function, int Value, bool GetStamina)
+        {
+            if (Function == 0)
+            {
+                Stamina += Value;
+            }
+            if (Function == 1)
+            {
+                Stamina -= Value;
+            }
+            if (Function == 2)
+            {
+                Stamina = Value;
+            }
+            if (Function == 3)
+            {
+                Stamina = Value;
+                BaseStamina = Stamina;
+            }
+            if (GetStamina)
+            {
+                Logger.LogInfo("GorrilaLifeStats Stamina is " + Stamina);
+            }
+        }
+        void Update()
+        {
+            if (Health > BaseHealth)
+            {
+                Health = BaseHealth;
+                Logger.LogWarning("Health was larger than BaseHealth. Setting to BaseHealth");
+            }
+            if (Health < 0)
+            {
+                Health = 0;
+                Logger.LogWarning("Health was smaller than 0, Setting Health to 0");
+            }
+            if (Stamina > BaseStamina)
+            {
+                Stamina = BaseStamina;
+                Logger.LogWarning("Stamina was larger than BaseStamina. Setting to BaseStamina");
+            }
+            if (Health < 0)
+            {
+                Health = 0;
+                Logger.LogWarning("Stamina was smaller than 0, Setting Stamina to 0");
+            }
+        }
+    }
+}
---
+++ GorillaLifeStats-PreConfigurated/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace GorillaLifeStats
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.pyluadotcode.gorillatag.gorillahealth";
+        public const string Name = "GorillaHealth";
+        public const string Version = "1.0.0";
+    }
+}
