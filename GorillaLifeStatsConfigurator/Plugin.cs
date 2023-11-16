e51de35c1259cc848fcf39d3d2122f7ca59f30ab
ConnorBieszk <147281703+pylua-dot-code@users.noreply.github.com>
2023-11-15 19:02:50 -0600

    Added The GC

---
+++ GorillaLifeStatsConfigurator/Plugin.cs
@@ -0,0 +1,52 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+using Utilla;
+using GorillaLifeStats;
+using BepInEx.Configuration;
+namespace GorillaLifeStatsConfigurator
+{
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInDependency("com.pyluadotcode.gorillatag.gorillahealth", "1.0.0")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+
+        bool inRoom;
+
+        void Start()
+        {
+            Utilla.Events.GameInitialized += OnGameInitialized;
+        }
+
+        void OnEnable()
+        {
+            HarmonyPatches.ApplyHarmonyPatches();
+        }
+
+        void OnDisable()
+        {
+            HarmonyPatches.RemoveHarmonyPatches();
+        }
+
+        private ConfigEntry<int> configHealth;
+        private ConfigEntry<bool> configStamina;
+
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+            configHealth = Config.Bind("Health", "BaseHealthSet", 100, "The base health");
+            configHealth = Config.Bind("Stamina", "BaseStaminaSet", 1000, "The base stamina");
+
+        }
+
+        public void OnJoin(string gamemode)
+        {
+            inRoom = true;
+        }
+
+        public void OnLeave(string gamemode)
+        {
+            inRoom = false;
+        }
+    }
+}
---
+++ GorillaLifeStatsConfigurator/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace GorillaLifeStatsConfigurator
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.pyluadotcode.gorillatag.gorillalifestatsconfigurator";
+        public const string Name = "GorillaLifeStatsConfigurator";
+        public const string Version = "1.0.0";
+    }
+}
